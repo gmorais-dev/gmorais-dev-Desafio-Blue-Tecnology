4d3b9f2a16ab3cbcbe7465069c9fd3b628093967
gmorais-dev <guilherme_morais14@hotmail.com>
2023-10-23 22:34:25 -0300

    Repository

--- Desafio Blue-Tecnology/Controllers/HomeController.cs
+++ Desafio Blue-Tecnology/Controllers/HomeController.cs
@@ -4,7 +4,25 @@ using System.Diagnostics;
 
 namespace Desafio_Blue_Tecnology.Controllers
 {
-}
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserServices services;
+        private readonly IRepositoryUser repository;
+
+        private readonly IMapper mapper;
+
+        public UserController(IUserServices services, IRepositoryUser repository)
+        {
+            this.services = services;
+            this.repository = repository;
+            this.mapper = mapper;
+
+        }
+    }
     
         
       
