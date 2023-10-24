3d715d89a8deede7948208c7e0e989f6be87b52c
gmorais-dev <guilherme_morais14@hotmail.com>
2023-10-23 21:57:37 -0300

    primeiro commit

---
+++ Desafio Blue-Tecnology/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Desafio_Blue_Tecnology.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Desafio_Blue_Tecnology.Controllers
+{
+}
+    
+        
+      
+        
+
+
+
+    
---
+++ Desafio Blue-Tecnology/Models/Contatos.cs
@@ -0,0 +1,13 @@
+namespace Desafio_Blue_Tecnology.Models
+{
+
+
+    public class Contatos
+    {
+        private  string? Name { get; set; }
+        private string? Telefone { get; set; }
+        private string? Email { get; set; }
+
+    
+    }
+}
